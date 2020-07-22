9686c1f28061516132d7224d17e48fb0fb3cce3d
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-22 17:25:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,10 @@
 def run_guessing_game
-  random_num = rand(5) + 1
+  random_num = (rand(5) + 1).to_s
   guess = gets.chomp
   case guess
   when "exit"
     puts "Goodbye!"
-  end
-  if guess.to_i === random_num
+  guess.to_i === random_num
     puts "You guessed the correct number!"
   end
   if guess != "exit" && guess.to_i != random_num
