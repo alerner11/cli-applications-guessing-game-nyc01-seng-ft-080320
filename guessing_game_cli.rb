f08c4eba60f74a1adef01e9738c8eccc92d5817d
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-22 17:24:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
   if guess.to_i === random_num
     puts "You guessed the correct number!"
   end
-  if guess != "exit" && guess != random_num
+  if guess != "exit" && guess.to_i != random_num
     puts "Sorry! The computer guessed #{random_num}."
   end
 end
